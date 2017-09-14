b904d22c1b26f8a5ca69be6140232e7cf7e308a8
kaghee <kass.agnes@gmail.com>
2017-09-14 10:39:40 +0200

    DrawSquare, ParametricAverage and DrawDiagonal uploaded

--- week-02/day-01/src/DrawSquare.java
+++ week-02/day-01/src/DrawSquare.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
 
-public class DrawDiagonal {
+public class DrawSquare {
     public static void main(String[] args) {
         // Write a program that reads a number from the standard input, then draws a
         // square like this:
         //
         //
         // %%%%%
-        // %%  %
-        // % % %
-        // %  %%
+        // %   %
+        // %   %
+        // %   %
+        // %   %
         // %%%%%
         //
         // The square should have as many lines as the number was
@@ -20,16 +21,16 @@ public class DrawDiagonal {
 
         String square = "";
         for (int i = 0; i < size; i++) {
-            if (i == 0 || i == size - 1) {                          // Top and bottom sides
+            if (i == 0 || i == size - 1) {              // Top and bottom sides
                 for (int j = 0; j < size; j++) {
                     square += "%";
                 }
             } else {
                 for (int j = 0; j < size; j++) {
-                    if (j == 0 || j == i || j == size - 1) {        // Left side, diagonal and right side
+                    if (j == 0 || j == size - 1) {      // Left and right sides
                         square += "%";
                     } else {
-                        square += " ";                              // Filling it up with SPAAAAACE
+                        square += " ";                  // Filling it up with SPAAAAACE
                     }
                 }
             }
