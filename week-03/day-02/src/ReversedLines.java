c37efe14bc1e739b6ba7ffce60088e2a95c0befd
kaghee <kass.agnes@gmail.com>
2017-09-19 14:42:08 +0200

    ReversedLines added

--- week-03/day-02/src/ReversedLines.java
+++ week-03/day-02/src/ReversedLines.java
@@ -1,8 +1,32 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReversedLines {
 
     public static void main(String[] args) {
         // Create a method that decrypts reversed-lines.txt
+        reverser("src/reversed-lines.txt");
+    }
 
-        
+    public static void reverser(String filename) {
+        List<String> contents = new ArrayList<>();
+        String newContent = "";
+        try {
+            Path myPath = Paths.get(filename);
+            contents.addAll(Files.readAllLines(myPath));
+            for (int i = 0; i < contents.size(); i++) {
+                for (int j = contents.get(i).length() - 1; j >= 0; j--) {
+                    newContent += contents.get(i).valueOf(contents.get(i).charAt(j));
+                }
+                newContent += "\n";
+            }
+            System.out.println(newContent);
+        } catch (IOException e) {
+            // no error msg
+        }
     }
 }
