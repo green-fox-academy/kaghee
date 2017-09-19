4571a3f9dd7435b9d29612124bdad68fcbf56957
kaghee <kass.agnes@gmail.com>
2017-09-19 14:45:55 +0200

    ReversedOrder added

--- week-03/day-02/src/ReversedLines.java
+++ week-03/day-02/src/ReversedLines.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ReversedLines {
-
     public static void main(String[] args) {
         // Create a method that decrypts reversed-lines.txt
         reverser("src/reversed-lines.txt");
--- week-03/day-02/src/ReversedOrder.java
+++ week-03/day-02/src/ReversedOrder.java
@@ -1,8 +1,31 @@
-public class ReversedOrder {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
+public class ReversedOrder {
     public static void main(String[] args) {
         // Create a method that decrypts reversed-order.txt
+        lineReverser("src/reversed-order.txt");
+    }
 
-        
+    public static void lineReverser(String filename) {
+        List<String> contents = new ArrayList<>();
+        String newContent = "";
+        try {
+            Path myPath = Paths.get(filename);
+            contents.addAll(Files.readAllLines(myPath));
+            for (int i = contents.size() - 1; i >= 0; i--) {
+                for (int j = 0; j < contents.get(i).length(); j++) {
+                    newContent += contents.get(i).valueOf(contents.get(i).charAt(j));
+                }
+                newContent += "\n";
+            }
+            System.out.println(newContent);
+        } catch (IOException e) {
+            // no error msg
+        }
     }
 }
