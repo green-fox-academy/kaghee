b20810fef8fd52cd585c14f786b0663b16c49048
kaghee <kass.agnes@gmail.com>
2017-10-24 13:32:49 +0200

    Web Greet counter app ready

--- week-07/day-02/springstart/src/main/java/com/greenfox/springstart/controllers/HelloWebController.java
+++ week-07/day-02/springstart/src/main/java/com/greenfox/springstart/controllers/HelloWebController.java
@@ -3,12 +3,19 @@ package com.greenfox.springstart.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class HelloWebController {
+    AtomicLong counter = new AtomicLong(1);
+
+    @ResponseBody
     @RequestMapping("/web/greeting")
-    public String greeting(Model model) {
-        model.addAttribute("name", " World");
-        return "greeting";
+    public String greeting(@RequestParam("name") String username) {
+        Greeting g = new Greeting(counter.getAndIncrement(), "Hello, " + username + "!");
+        return g.content + " This site was loaded " + g.id + " times since last server start.";
     }
 }
