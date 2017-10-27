8ad3b5b9d6993dcbb9564a478d795594d357a81e
kaghee <kass.agnes@gmail.com>
2017-10-27 14:04:11 +0200

    navigating btw index and learning pages working, tricks added

--- week-07/day-05/foxclub/src/main/java/com/greenfox/kaghee/foxclub/foxclub/controller/MainController.java
+++ week-07/day-05/foxclub/src/main/java/com/greenfox/kaghee/foxclub/foxclub/controller/MainController.java
@@ -5,7 +5,9 @@ import com.greenfox.kaghee.foxclub.foxclub.model.Trick;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
@@ -16,11 +18,22 @@ public class MainController {
     @Autowired
     Trick trick;
 
-    @RequestMapping(value="/")
+    @GetMapping(value="/")
     public String index(Model model) {
         model.addAttribute("fox", fox);
-        model.addAttribute("zoli", fox.getNumOfTricks());
-
+        model.addAttribute("trick", trick);
         return "index";
+    }
+
+    @GetMapping(value="/learn")
+    public String learn() {
+        return "learn";
+    }
+
+    @PostMapping("/learn")
+    public String learnTrick(@ModelAttribute Trick trick) {
+        this.trick = trick;
+        fox.learnTrick(trick.getDescription());
+        return "redirect:/";
     }
 }
--- week-07/day-05/foxclub/src/main/java/com/greenfox/kaghee/foxclub/foxclub/model/Trick.java
+++ week-07/day-05/foxclub/src/main/java/com/greenfox/kaghee/foxclub/foxclub/model/Trick.java
@@ -1,14 +1,22 @@
 package com.greenfox.kaghee.foxclub.foxclub.model;
 
 public class Trick {
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     String description;
 
     public Trick(){
 
     }
-    
+
     public Trick(String description) {
         this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
 }
