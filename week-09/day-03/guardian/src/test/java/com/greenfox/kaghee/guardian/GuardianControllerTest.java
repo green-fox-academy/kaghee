462dea3730e1f8ad78b8502be06851f9e065dece
kaghee <kass.agnes@gmail.com>
2017-11-08 15:12:29 +0100

    groot task done with testing

---
+++ week-09/day-03/guardian/src/test/java/com/greenfox/kaghee/guardian/GuardianControllerTest.java
@@ -0,0 +1,63 @@
+package com.greenfox.kaghee.guardian;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = GuardianApplication.class)
+@WebAppConfiguration
+@EnableWebMvc
+public class GuardianControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setup() throws Exception {
+        this.mockMvc = webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void grootTestWithParameter() throws Exception {
+        mockMvc.perform(get("/groot?message=Hellomi")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.translated", is("I am Groot!")))
+                .andExpect(jsonPath("$.received", is("Hellomi")));
+    }
+
+    @Test
+    public void grootTestWithoutParameter() throws Exception {
+        mockMvc.perform(get("/groot")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.error", is("I am Groot!")));
+    }
+
+    @Test
+    public void grootTestWith404() throws Exception {
+        mockMvc.perform(get("/groot")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
+    }
+
+}
